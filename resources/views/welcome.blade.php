babe56d0ef3e843dd1aa3394649cb950e298554d
Dipo Sukmana <diposukmana@gmail.com>
2024-09-05 23:49:56 +0800

    "Updated package name, removed font import, modified CSS and Blade templates, and changed Tailwind config"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,64 @@
-@section('title')
+@vite('resources/css/navbar.css')
+@vite('resources/css/welcome.css')
+
+<body>
+    <header>
+        @if (Route::has('login'))
+            <nav>
+                <div class="left-nav">
+                    <a href="">Ini Logo</a>
+                </div>
+                <div class="right-nav">
+                    <div class="menu">
+                        <a href="#">Home</a>
+                        <a href="#">Our Services</a>
+                        <a href="#">Let's Talk</a>
+                        <a href="#">Community</a>
+                        <a href="#">Contact Us</a>
+                    </div>
+                    <div class="auth">
+                        @auth
+                            <a href="{{ url('/dashboard') }}">Dashboard</a>
+                            <form action="{{ route('logout') }}" method="post">
+                                @csrf 
+                                <button type="submit" class="button">Logout</button>
+                            </form>
+                        @else
+                            <p>{{Auth::check()}}</p>
+
+                            <a href="{{ route('login') }}">Login</a>
+
+                            @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                            @endif
+                    </div>
+                </div>
+            </nav>
+        @endif
+        @endauth
+</header>
+
+<main>
+    <div class="hero">
+        <div class="hero-title">
+            <p>your creative partner</p>
+            <h1>simplify your messy technical production</h1>  
+        </div>
+        <div class="desc">
+            <p>boost creative productivity: webtoons, video editing, social media</p>
+        </div>
+    </div>
+</main>
+
+</body>
+
+
+<!-- @section('title')
     Meals on Wheels
 @endsection
-@vite('resources/css/welcome.css')
+@vite('resources/css/welcome.css') -->
 <!-- @vite('resources/css/flying.css') -->
-<script src="{{ asset('js/components/welcome.js')}}"></script>
+<!-- <script src="{{ asset('js/components/welcome.js')}}"></script>
 
 <style>
     .hiden{
@@ -18,12 +73,10 @@
         filter: blur(0);
         transform: translateX(0);
     }
-</style>
-@extends('layouts.app')
-@section('content')
+</style> -->
+<!-- @extends('layouts.app')
 
-<div>hallo </div>
 
-</script>
 
-@endsection
+
+
